4245a57745397477e7dd04b708a7269a4b6c99d1
⠛⠥⠍⠊⠇⠇⠕ <spx.kuba@gmail.com>
2026-01-20 16:21:00 +0100

    Add task 4.4 - Complete game logic with random computer move and user input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,52 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+// Losowanie ruchu komputera
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+// Ruch gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+// Wynik gry
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Tym razem przegrywasz :(');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Tym razem przegrywasz :(');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Tym razem przegrywasz :(');
+} else if( computerMove == playerMove){
+  printMessage('Remis!');
+} else if( playerMove == 'nieznany ruch'){
+  printMessage('Błąd! Wpisz liczbę 1, 2 lub 3.');
+}
